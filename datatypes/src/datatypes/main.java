4c4b999e8ae2bd3040eadf2eedaf937455ed0fc0
Ali Karçak <89981493+Ali2718@users.noreply.github.com>
2022-09-21 13:42:41 +0300

    Add files via upload

---
+++ datatypes/src/datatypes/main.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		double sayi = 12.5;
+		sayi = -129;
+		char Karakter = 'A';
+		
+		boolean dogruMu=false;
+	
+
+	}
+
+}
